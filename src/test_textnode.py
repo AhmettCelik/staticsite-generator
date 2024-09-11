f688a11b52fc290c22dbca031915f21dca0cff60
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2024-09-11 18:30:09 +0300

    my solution and test codes

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,7 @@ from textnode import (
     text_type_image,
     text_type_link,
     text_node_to_html_node,
+    split_nodes_delimiter
 )
 
 
@@ -63,6 +64,62 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "This is bold")
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_code(self):
+        node = TextNode("This is text with a `code block` word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        self.assertEqual(new_nodes, 
+			[
+				TextNode("This is text with a ", text_type_text),
+				TextNode("code block", text_type_code),
+				TextNode(" word", text_type_text),
+			]
+        )
+
+    def test_multiple_nodes(self):
+        nodes = [
+            TextNode("This is text with a `code block` word, this is another `code block` word", text_type_text),
+            TextNode("`This is text with a code block word`", text_type_text),
+            TextNode("This is text with a `code block` word too", text_type_text),
+        ]
+        new_nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+        self.assertEqual(new_nodes, 
+			[
+				TextNode("This is text with a ", text_type_text),
+				TextNode("code block", text_type_code),
+				TextNode(" word, this is another ", text_type_text),
+				TextNode("code block", text_type_code),
+				TextNode(" word", text_type_text),
+				TextNode("This is text with a code block word", text_type_code),
+				TextNode("This is text with a ", text_type_text),
+				TextNode("code block", text_type_code),
+				TextNode(" word too", text_type_text),
+			]
+        )
+
+    
+    def test_bold(self):
+        node = TextNode("This is text with a **bold block** word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        self.assertEqual(new_nodes, 
+			[
+				TextNode("This is text with a ", text_type_text),
+				TextNode("bold block", text_type_bold),
+				TextNode(" word", text_type_text),
+			]
+        )
+
+    def test_italic(self):
+        node = TextNode("This is text with a *italic block* word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "*", text_type_italic)
+        self.assertEqual(new_nodes, 
+			[
+				TextNode("This is text with a ", text_type_text),
+				TextNode("italic block", text_type_italic),
+				TextNode(" word", text_type_text),
+			]
+        )
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -39,3 +39,20 @@ def text_node_to_html_node(text_node):
 
     raise ValueError(f"Invalid text type: {text_node.text_type}")
 
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    for node in old_nodes:
+        if delimiter not in node.text:
+            raise Exception("That's invalid Markdown syntax")
+    splitted_nodes = list(map(lambda node: node.text.split(delimiter), old_nodes))
+    final_list = []
+    for node in splitted_nodes:
+        text_index = 0
+        for text in node:
+            if text != "":
+                if text_index % 2 == 0:
+                    final_list.append(TextNode(text, text_type_text))
+                else:
+                    final_list.append(TextNode(text, text_type))
+            text_index += 1
+    return final_list
+
