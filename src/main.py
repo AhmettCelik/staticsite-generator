1093fcac7f8da95c527843e6fbc17fe58075f6d5
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2024-09-15 16:58:50 +0300

    finally

--- src/main.py
+++ src/main.py
@@ -9,6 +9,11 @@ dir_path_content = "./content"
 template_path = "./template.html"
 
 def main():
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
     copy_directory_recursive(dir_path_static, dir_path_public)
     generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
@@ -28,33 +33,6 @@ def copy_directory_recursive(src, dest):
             shutil.copy(src_path, dest_path)
             print(f'Copied file: {src_path} -> {dest_path}')
             
-def extract_title(markdown):
-    lines = markdown.split("\n")
-    h1 = [line for line in lines if line.startswith("# ")]
-    if len(h1) == 0:
-        raise Exception("need least one h1")
-    return h1[0]
-
-def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
-    
-    with open(from_path, 'r') as from_file:
-        from_path_as_string = from_file.read()
-        
-    with open(template_path, 'r') as template_file:
-        template_path_as_string = template_file.read()
-        
-    HTML = markdown_to_html_node(from_path_as_string).to_html()
-    page_title = extract_title(from_path_as_string)
-    
-    new_template_path_as_string = template_path_as_string.replace("{{ Title }}", page_title)
-    new_template_path_as_string = new_template_path_as_string.replace("{{ Content }}", HTML)
-    
-    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-    
-    with open(dest_path, 'w') as dest_file:
-        dest_file.write(new_template_path_as_string)
-
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
@@ -64,5 +42,37 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             generate_page(from_path, template_path, dest_path)
         else:
             generate_pages_recursive(from_path, template_path, dest_path)
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f" * {from_path} {template_path} -> {dest_path}")
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
+
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
+
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
+
+    title = extract_title(markdown_content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
+
+
+def extract_title(md):
+    lines = md.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("No title found")
             
 main()
